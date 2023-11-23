1c71f03d43cc0ec373068b4fa741def1dd59f7ee
dev-ng <astroniki@gmail.com>
2023-11-23 12:09:55 +0200

    Setup Router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Starter template</p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => <div>Dashboard Component</div>;
+const AddExpensePage = () => <div>Add Expense Page</div>;
+const EditExpensePage = () => <div>Edit Expense Page</div>;
+const HelpPage = () => <div>Help Page</div>;
+const NotFoundPage = () => <div>404 Page</div>;
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
